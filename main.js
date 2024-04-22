5ae0cdfbd791629dceff2742c943652f9242eb50
Fabian Silva <fsilva@directionsresearch.com>
2024-04-22 15:41:08 -0400

    finish lesson 5

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 22345, color: "green" },
+        { id: 22345, color: "blue" },
+      ],
+      sizes: ["small", "medium", "large"],
+    };
+  },
+});
